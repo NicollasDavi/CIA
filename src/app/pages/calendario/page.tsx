8bf7c4f829c3b9d9591db6f2cb46496678853faa
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-04-29 18:22:29 -0300

    .

--- src/app/pages/calendario/page.tsx
+++ src/app/pages/calendario/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 "use client"
 import { SetStateAction, useState } from 'react';
 import { format } from 'date-fns';
--- src/components/PegarDia.tsx
+++ src/components/PegarDia.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 "use client"
 import React, { useState } from 'react';
 import { DayPicker } from 'react-day-picker';
